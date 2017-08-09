afded80d1650823cf1343929a1ac6978dffbeeda
Sun Lau <sun.lau@iheha.com>
2017-08-09 13:15:22 +0800

    mobile view

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Booking System</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="script/vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="script/vendor/bootstrap-calendar-master/css/calendar.css">
 </head>
